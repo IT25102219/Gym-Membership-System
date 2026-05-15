fdc2a7766a18b051604825e67fe24e40a29e12e7
Rukshana Perinparajah <it25102637@my.sliit.lk>
2026-05-15 15:31:27 +0530

    Add GymApplication and checkIn.jsp for attendance management interface

---
+++ src/main/java/com/gym/GymApplication.java
@@ -0,0 +1,21 @@
+package com.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@ServletComponentScan
+public class GymApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GymApplication.class);
+    }
+}
